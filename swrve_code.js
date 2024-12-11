0b5ee3378bf97122c60c6731507e42b2600367a2
CiaranMcCann <ciaran.mccann@swrve.com>
2024-12-11 10:19:20 +0000

    Workaround for demo

--- swrve_code.js
+++ swrve_code.js
@@ -1,31 +1,41 @@
 // Create a shortcut to the inner SwrveSDK object
-       const Swrve = SwrveSDK.SwrveSDK;
+const Swrve = SwrveSDK.SwrveSDK;
 
 
-    Swrve.initWithConfig({
-             appId: 1030,
-             apiKey: "web_sdk-xtTPIygFc0kbEKxP73U", //apiKey: "web_sdk-z4O2WrkxykZTuZtSglXt",
-             externalUserId: "ciaran_testing_1",
-             stack: "us", 
-             autoPushSubscribe: true,
-             userVisibleOnly: true,
-             serviceWorker: './SwrveWorker.js'
-         });
+Swrve.initWithConfig({
+    appId: 1030,
+    apiKey: "web_sdk-xtTPIygFc0kbEKxP73U", //apiKey: "web_sdk-z4O2WrkxykZTuZtSglXt",
+    stack: "us",
+    autoPushSubscribe: true,
+    userVisibleOnly: true,
+    serviceWorker: './SwrveWorker.js'
+});
 
-    // Add to cart button send event and grab product information.
-          const addToCartButtons = document.querySelectorAll('.add_to_cart');
-          addToCartButtons.forEach((button) => {
-              button.addEventListener('click', () => {
-                  // Find the associated input with the class 'product-variant-id'
-                  const variantInput = 192
-      
-                  if (variantInput) {
-                      const itemId = variantInput.value; // Get the value of the input field
-                      SwrveSDK.SwrveSDK.event("add_to_cart", { "item_id": itemId });
-                      console.log("add_to_cart")
-                      window.location = "./cart.html"
-                  } else {
-                      console.error("Product variant input not found");
-                  }
-              });
-          });
+const addToCartButtons = document.querySelectorAll('.add_to_cart');
+addToCartButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const variantInput = 192
+
+        if (variantInput) {
+            const itemId = variantInput.value; 
+            SwrveSDK.SwrveSDK.event("add_to_cart", {
+                "item_id": itemId
+            });
+            console.log("add_to_cart")
+        } else {
+            console.error("Product variant input not found");
+        }
+    });
+});
+
+
+const currentParams = window.location.search;
+const links = document.querySelectorAll('a');
+
+links.forEach(link => {
+    const href = link.getAttribute('href');
+    if (href && !href.startsWith('#') && currentParams) {
+        const separator = href.includes('?') ? '&' : '?';
+        link.setAttribute('href', `${href}${separator}${currentParams.substring(1)}`);
+    }
+});
